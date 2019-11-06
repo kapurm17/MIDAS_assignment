f644411766d02e9cae91cf066d52944a54e74879
mayank-kapur <kapurm17@gmail.com>
2019-11-06 09:41:15 +0530

    Add files via upload

---
+++ SPEECH EMOTION PROBLEM/script.sh
@@ -0,0 +1,7 @@
+conda create -n new_env -y pip python=3.6
+conda init bash
+#conda activate new_env
+#conda install -c conda-forge -y tensorflow
+#conda install -c conda-forge -y keras
+conda install -n new_env -y keras
+conda activate new_env
